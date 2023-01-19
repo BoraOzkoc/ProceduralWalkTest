91dd5a67056276dca9e70e0e692768eb5a33121c
BoraOzkoc <48334969+BoraOzkoc@users.noreply.github.com>
2023-01-19 20:31:18 +0300

    ProceduralAnim completed

--- Assets/LegController.cs
+++ Assets/LegController.cs
@@ -5,27 +5,90 @@ using UnityEngine;
 
 public class LegController : MonoBehaviour
 {
-    [SerializeField] private Transform target;
+    [SerializeField] private Transform target, hint;
+    [SerializeField] private LayerMask layerMask;
     [SerializeField] private float threshHold;
-    private Vector3 _targetPos;
+    [SerializeField] private int id;
+    private Vector3 _targetPos, defaultLegPosition;
+    private bool _legPlaced;
+    private Coroutine _positionCoroutine;
+    private SpiderController _spiderController;
 
-    public void Init()
+    public void Init(SpiderController spiderController)
     {
+        _spiderController = spiderController;
         _targetPos = target.localPosition;
+        defaultLegPosition = transform.InverseTransformPoint(_targetPos);
     }
 
     private void FixedUpdate()
     {
-        //CheckLegDistance();
+        if (_legPlaced)
+        {
+            CheckLegDistance(RayCast());
+        }
+        else
+        {
+            _spiderController.JoinQueue(this);
+        }
+
+        target.position = _targetPos;
     }
 
-    private void CheckLegDistance()
+    private void CheckLegDistance(Vector3 pos)
     {
-        Vector3 pos = transform.TransformPoint(_targetPos);
-
-        if (Vector3.Distance(target.position,pos) > threshHold)
+        Debug.Log("distance checking");
+        if (Vector3.Distance(pos, target.position) > threshHold)
         {
-            target.position = pos;
+            Debug.Log("too far");
+
+            _legPlaced = false;
         }
+    }
+
+    private Vector3 RayCast()
+    {
+        RaycastHit hitData;
+
+        Ray ray = new Ray(hint.position, (transform.TransformPoint(defaultLegPosition) - hint.position).normalized);
+
+        if (Physics.Raycast(ray, out hitData, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(ray.origin, ray.direction, Color.green);
+            return hitData.point;
+        }
+        else
+        {
+            Debug.DrawRay(ray.origin, ray.direction * .75f, Color.red);
+
+            return ray.GetPoint(.75f);
+        }
+    }
+
+    public void PositionLeg()
+    {
+        IEnumerator PositionCoroutine()
+        {
+            Vector3 pos = RayCast();
+
+            Vector3 risePoint = ((_targetPos + pos) / 2) + (Vector3.up * 0.05f);
+            _targetPos = risePoint;
+            _legPlaced = true;
+
+            yield return new WaitForSeconds(0.05f);
+
+            _targetPos = pos;
+            Debug.Log("leg positioned");
+        }
+
+        // if (_positionCoroutine != null) StopCoroutine(_positionCoroutine);
+
+        _positionCoroutine = StartCoroutine(PositionCoroutine());
+
+        // if (Vector3.Distance(target.position, pos) > threshHold)
+        // {
+        //     target.position = pos;
+        //     _targetPos = target.localPosition;
+        // }
     }
 }
--- Assets/SpiderController.cs
+++ Assets/SpiderController.cs
@@ -1,21 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpiderController : MonoBehaviour
 {
     [SerializeField] private List<LegController> legList = new List<LegController>();
+    [SerializeField] private Queue<LegController> legQueue = new Queue<LegController>();
 
-    
+    private Coroutine _queueCoroutine;
+
     public void Init()
     {
         for (int i = 0; i < legList.Count; i++)
         {
-            legList[i].Init();
-
+            legList[i].Init(this);
         }
     }
 
 
-    
+    public void JoinQueue(LegController legController)
+    {
+        if (!legQueue.Contains(legController))
+        {
+            legQueue.Enqueue(legController);
+
+            Dequeue();
+        }
+    }
+
+    private void Dequeue()
+    {
+        IEnumerator DequeueCoroutine()
+        {
+            while (legQueue.Count > 0)
+            {
+                legQueue.Dequeue().PositionLeg();
+
+                yield return new WaitForSeconds(0.01f);
+            }
+
+            if (_queueCoroutine != null) _queueCoroutine = null;
+        }
+
+        if (_queueCoroutine == null) _queueCoroutine = StartCoroutine(DequeueCoroutine());
+    }
 }
